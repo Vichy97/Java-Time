39b8cf8cd2340d4fd008f1df1347a4b513bc2c7b
Vincent <VincentWilliams97@gmail.com>
2019-11-10 17:05:02 -0700

    Setup drawer layout

--- app/src/main/java/com/vincent/javatime/MainActivity.kt
+++ app/src/main/java/com/vincent/javatime/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.vincent.javatime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +18,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_JavaTime)
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
         setupToolbar()
+        //setupDrawerLayout()
     }
 
     private fun setupToolbar() {
         setSupportActionBar(toolbar)
-        toolbar.setupWithNavController(navController)
+        //setupActionBarWithNavController(navController, drawer_layout)
     }
+
+    private fun setupDrawerLayout() {
+        nav_view.setupWithNavController(navController)
+
+        nav_view.setNavigationItemSelectedListener {
+
+            drawer_layout.closeDrawers()
+
+            when (it.itemId) {
+                R.id.menu_item_about -> {
+                    navController.navigate(R.id.aboutFragment)
+                    true
+                }
+                R.id.menu_item_favorites -> {
+                    navController
+                    true
+                }
+                else -> { false }
+            }
+        }
+    }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp(drawer_layout)
+//    }
 }
